df1806d6108a0fd6b6f7ebd422596f7f205336de
Opeyemi <eco.ajibade@gmail.com>
2025-09-12 00:30:58 +0100

    0x04-TypeScript

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,21 +1,16 @@
-// task_3/js/main.ts
 /// <reference path="./crud.d.ts" />
 
 import { RowID, RowElement } from './interface';
 import * as CRUD from './crud.js';
 
 const row: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva'
+  firstName: "Guillaume",
+  lastName: "Salva"
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
 
-const updatedRow: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-  age: 23,
-};
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
